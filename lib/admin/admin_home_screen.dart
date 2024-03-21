617ad922b40cb31d0a81ecaa121adadcc18056c4
Saumil Vijaybhai Hirpara <sonupatel00170@gmail.com>
2024-03-21 11:32:21 +0530

    admin logout complete

--- lib/admin/admin_home_screen.dart
+++ lib/admin/admin_home_screen.dart
@@ -5,6 +5,8 @@ import 'package:saumil_s_application/admin/admin_employer_screen.dart';
 import 'package:saumil_s_application/admin/admin_job_screen.dart';
 import 'package:saumil_s_application/admin/admin_jobseeker_screen.dart';
 
+import '../presentation/sign_up_create_acount_screen/sign_up_create_acount_screen.dart';
+
 class AdminHomeScreen extends StatefulWidget {
   const AdminHomeScreen({Key? key}) : super(key: key);
 
@@ -31,6 +33,50 @@ class _AdminHomeScreenState extends State<AdminHomeScreen> {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
+        // appBar: AppBar(
+        //   backgroundColor: Theme.of(context).primaryColor,
+        //   automaticallyImplyLeading: false, // Remove back button
+        //   title: Row(
+        //     children: [
+        //       Text(
+        //         'Admin Dashboard',
+        //         style: TextStyle(
+        //           fontSize: 20.0,
+        //           fontWeight: FontWeight.bold,
+        //           color: Colors.white, // Set the text color to white
+        //         ),
+        //       ),
+        //       Spacer(),
+        //     ],
+        //   ),
+        //   bottom: TabBar(
+        //     indicatorColor: Colors.white,
+        //     labelColor: Colors.white,
+        //     unselectedLabelColor: Colors.grey,
+        //     onTap: (index) {
+        //       setState(() {
+        //         _selectedIndex = index;
+        //         selectInt.value = _selectedIndex;
+        //       });
+        //     },
+        //     tabs: [
+        //       Tab(
+        //         icon: Icon(Icons.group),
+        //         text: 'Job Seekers',
+        //       ),
+        //       Tab(
+        //         icon: Icon(Icons.business),
+        //         text: 'Employers',
+        //       ),
+        //       Tab(
+        //         icon: Icon(Icons.work),
+        //         text: 'Posted Jobs',
+        //       ),
+        //     ],
+        //   ),
+        // ),
+
+
         appBar: AppBar(
           backgroundColor: Theme.of(context).primaryColor,
           automaticallyImplyLeading: false, // Remove back button
@@ -47,6 +93,15 @@ class _AdminHomeScreenState extends State<AdminHomeScreen> {
               Spacer(),
             ],
           ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                // Implement your logout logic here
+                Get.to(SignUpCreateAcountScreen());
+              },
+              icon: Icon(Icons.logout, color: Colors.white),
+            ),
+          ],
           bottom: TabBar(
             indicatorColor: Colors.white,
             labelColor: Colors.white,
